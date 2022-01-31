a48aea5c0933d7af2c819fc2025e4e737d0cf725
gaukha <96620616+gaukha@users.noreply.github.com>
2022-01-31 17:59:08 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function multiply()
+{
+        num1=document.getElementById("firstNum").value;
+        num2=document.getElementById("secondNum").value;
+        document.getElementById("result").innerHTML = num1*num2;
+}
+function divide()
+{
+        num1=document.getElementById("firstNum").value;
+        num2=document.getElementById("secondNum").value;
+document.getElementById("result").innerHTML=num1/num2;
+}
